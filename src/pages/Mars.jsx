c1c46491831881c8b994b2459b374206b5f91d02
Callum Hall <callumhall@Callums-MacBook-Pro.local>
2022-12-16 17:28:34 +0000

    test with new componenets

--- src/pages/Mars.jsx
+++ src/pages/Mars.jsx
@@ -1,7 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import CreateDateInput from "../Components/DateInput.jsx";
+import CreateImgContainer from "../Components/ImgContainer";
 
 const MarsPage = () => {
+  const [DateInputMars, SetMarsDateInput] = useState(null);
+  const [CurrentInputMars, SetMarsCurrentInput] = useState("");
+  const [marsSRC, setMarsSRC] = useState(null);
+  const handleMarsDate = (e) => {
+    const marsDate = e.target.value;
+    SetMarsCurrentInput(marsDate);
+  };
+
+  const InputDateMars = () => {
+    SetMarsDateInput(CurrentInputMars);
+    console.log(DateInputMars);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [DateInputMars]);
+
+  const fetchData = async () => {
+    try {
+      const res = await fetch(
+        `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${DateInputMars}&camera=fhaz&api_key=88u7Vdtn2mchlreadL3NV33PEHngIuuEStrURSXH`
+      );
+
+      const data = await res.json();
+      // console.log(data);
+      setMarsSRC(data["photos"][0]["img_src"]);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
   return (
     <div>
       <div>
@@ -15,8 +47,10 @@ const MarsPage = () => {
         <br />
         <CreateDateInput
           inputDate={InputDateMars}
-          handleDateChange={handleMarsDate}
+          handleDataChange={handleMarsDate}
         />
+
+        <CreateImgContainer src={marsSRC} />
       </div>
     </div>
   );
