fa02024b64154d26982d843c4459227b5c509161
Callum Hall <callumhall@Callums-MacBook-Pro.local>
2023-01-04 15:53:49 +0000

    changes to homepage

--- src/pages/HomeP.jsx
+++ src/pages/HomeP.jsx
@@ -1,10 +1,42 @@
 import React from "react";
 import "../CSS/Home.css";
+import background from "../Assets/Astro.png";
+import rock1 from "../Assets/Rock0027.png";
+import rock2 from "../Assets/Rock0028.png";
+import stars from "../Assets/stars.png";
+import planet from "../Assets/Planet.png";
 
 const HomePage = ({ signInCredentials }) => {
   return (
     <div className="wrapper">
       <div className="home-div">
+        <div className="space-pictures">
+          <img className="spaceman" src={background}></img>
+
+          <img className="rock" src={rock1}></img>
+          <img className="rock1" src={rock2}></img>
+        </div>
+        <div className="title-text">
+          <h1
+            className="my-5 display-2 fw-bold ls-tight px-5"
+            style={{ color: "hsl(218, 81%, 95%)" }}
+          >
+            Space Explorer <br />
+            <span className="title-text-nasa">NASA API</span>
+          </h1>
+
+          <p className="px-1" style={{ color: "hsl(218, 81%, 85%)" }}>
+            Space Explorer uses an API created by NASA! This powerful API
+            enables us to view pictures from outer space with a click of a
+            button
+          </p>
+
+          <p className="px-2" style={{ color: "hsl(218, 81%, 85%)" }}>
+            This website enables you to select any date, ranging from the early
+            2000's up to recent times, and to retrieve a picture Nasa took on
+            that day{" "}
+          </p>
+        </div>
         {/* <h1 className="tracking-in-expand">WELCOME TO SPACE EXPLORER </h1>
         <div className="flex-container-home">
           <div className="flex-container-child">
